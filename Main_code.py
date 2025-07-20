52b6e889a434208cfdb37754f6c0fe783fe05610
LeonDellaMea <leonardo.dellamea@edu.unige.it>
2025-07-20 20:25:55 +0200

    Update Main_code.py

--- Main_code.py
+++ Main_code.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sun Jul 20 11:01:09 2025
 
@@ -19,14 +18,23 @@ import ANS_fun
 '''
 
 
-The phantom network is defined as follows
+The phantom network is defined as follows:
+    
+    
+         S          S          S   
+    N -------> N -------> N -------> N 
+        \\         \\         \\
+        \\         \\         \\
+        A <------> A <-------> A
 
 
 
-
-
-
-
+N = neuron;
+S = synapse;
+A = astrocyte;
+\\ = Bidirectional vescicle exocitosys-driven connections between astrocytes and synapses;
+<--> = Gap-junction mediated links between astrocytes;
+--> = Synaptic connections.
 
 
 
@@ -86,11 +94,11 @@ oscillations = 'AM'
 # ------------------------- PARAMETERS -------------------------
 
 # --------- SIMULATION -----------
-simtime = 100 * second               # simulation time
-transient = 3 * second              # time omitted as transient
+simtime = 2 * second               # simulation time
+# transient = 3 * second              # time omitted as transient
 sed = 39                             # random number seed
 devices.device.seed(sed)            # set the seed for all the random number realisations
-dt = defaultclock.dt 
+
 
 Simulated_network = 'Neuronal' # Astrocytic/Neuronal/Full
 
@@ -100,13 +108,24 @@ Nn = 4
 
 
 # --------- SYNAPTIC -----------
+
 # ----- Connectivity -----
-ADJ_neuro
+ADJ_neuro = np.array(([0,1,0,0],
+                      [0,0,1,0],
+                      [0,0,0,1],
+                      [0,0,0,0]))
 
 
 # --------- ASTROCYTE and GJ ----------- 
+'''
+Given the nature of the link hte adjency matrix is ALWAYS symmetric
+
+'''
 Na = 3
-ADJ_astro = 
+ADJ_astro = np.array(([0,1,0],
+                      [1,0,1],
+                      [0,1,0]))
+
 
 
 # --------- GLIOTRANSMISSION ----------- 
@@ -114,11 +133,15 @@ ADJ_astro =
 
 # --------- ASTRO-NEURON LINKS ----------- 
 # --- Synapse to astro ---
-ADJ_SynAstro = 
+ADJ_SynAstro = np.array(([1,0,0],
+                         [0,1,0],
+                         [0,0,1]))
 
 
 # --- Astro to synapse ---
-ADJ_AstroSyn = 
+ADJ_AstroSyn = np.array(([1,0,0],
+                         [0,1,0],
+                         [0,0,1]))
 
 
 
@@ -132,13 +155,13 @@ ADJ_AstroSyn =
 
 if Simulated_network == 'Full':
     # --------- NEURON and SYNAPSE -----------
-    N,S = Neuronal_Network(Nn,Source_neuron,Target_neuron, RandomKinetics, OnlyExc,
+    N,S = Neuronal_Network(Nn,ADJ_neuro, RandomKinetics, OnlyExc,
                            Syn_Currents_model,add_delay,delay_mode,Max_delay,ics,
                            std_pers, Simulated_network)
     
     
     # --------- ASTROCYTE -----------
-    A,GJ = Astrocyte_Group(N_astro,Source_astro,Target_astro)
+    A,GJ = Astrocyte_Group(N_astro,ADJ_astro)
     
     
     # --------- GLIOTRANSMISSION ----------- 
@@ -147,10 +170,10 @@ if Simulated_network == 'Full':
     
     # --------- ASTRO-NEURON LINKS ----------- 
     # --- Synapse to astro ---
-    StoA = Synapse_to_astro(S,A,Pre_syn,Post_astro)
+    StoA = Synapse_to_astro(S,A,ADJ_SynAstro)
     
     # --- Astro to synapse ---
-    AtoS = Astro_to_Syn(GT,S,Pre_astro,Post_syn)
+    AtoS = Astro_to_Syn(GT,S,ADJ_AstroSyn)
     
     
     
@@ -158,17 +181,19 @@ if Simulated_network == 'Full':
 elif Simulated_network == 'Neuronal':
 
     # --------- NEURON and SYNAPSE -----------
-    N,S = Neuronal_Network(Nn,Source_neuron,Target_neuron, RandomKinetics, OnlyExc,
+    N,S = Neuronal_Network(Nn,ADJ_neuro, RandomKinetics, OnlyExc,
                            Syn_Currents_model,add_delay,delay_mode,Max_delay,ics,
                            std_pers, Simulated_network)
     
     
     
-elif Simulated_network == 'Atrocytic':   
+elif Simulated_network == 'Astrocytic':   
     
     # --------- ASTROCYTE -----------
-    A,GJ = Astrocyte_Group(N_astro,Source_astro,Target_astro)
+    A,GJ = Astrocyte_Group(N_astro,ADJ_astro)
     
+    # Add some external stimulation through Y_bias
+    A.Y_bias[0] = 1 * mmolar 
     
     
     
@@ -182,11 +207,171 @@ elif Simulated_network == 'Atrocytic':
 # ------------------------- NETWORK SIMULATION -------------------------
 
 # --- Monitors ---
-recording_stringP = ['V']
-recording_stringS = ['u_S','x_S','Y_S','I_syn','I_ampa','I_nmda']
+recording_stringN = ['V','I_syn','I_ampa','I_nmda']
+recording_stringS = ['u_S','x_S','Y_S']
 recording_stringA = ['C','I','Gamma_A','I_coupling_tot']
 recording_stringGT = ['G_A','x_A']
 
 
-# collect
+if Simulated_network == 'Full':
+    MonitorS = StateMonitor(S, recording_stringS, record=True)
+    MonitorA = StateMonitor(A, recording_stringA, record=True)
+    MonitorN = StateMonitor(N, recording_stringN, record=True)
+    SpikesN = SpikeMonitor(N)
+    SpikesA = SpikeMonitor(A)
+
+elif Simulated_network == 'Neuronal':
+    MonitorS = StateMonitor(S, recording_stringS, record=True)
+    MonitorN = StateMonitor(N, recording_stringN, record=True)
+    SpikesN = SpikeMonitor(N)
+    
+    
+elif Simulated_network == 'Astrocytic': 
+    MonitorA = StateMonitor(A, recording_stringA, record=True)
+    SpikesA = SpikeMonitor(A)
+
+
+# --- Collect and add monitors ---
+
+
+net_ = Network(collect())  # automatically include all the stated groups
+net_.run(simtime,report='text', profile=True)
+
+
+
+#%%
+spike_trains = SpikesN.spike_trains()
+# --------------------- PLOTS ---------------------
+# ------- NEURONS -------
+
+fig, (ax1, ax2, ax3,ax4) = plt.subplots(4, 1,figsize=[10,12]) # Added figsize for better viewing
+
+
+
+ax1.plot(MonitorN.t / second, MonitorN[0].V / mV, 'k', linewidth=0.7)
+ax1.set_ylabel('Voltage [mV]')
+
+ax2.plot(MonitorN.t / second, MonitorN[1].V / mV, 'k', linewidth=0.7)
+ax2.set_ylabel('Voltage [mV]')
+
+
+
+ax3.plot(MonitorN.t / second, MonitorN[2].V / mV, 'k', linewidth=0.7)
+ax3.set_ylabel('Voltage [mV]')
+
+
+
+ax4.plot(MonitorN.t / second, MonitorN[3].V / mV, 'k', linewidth=0.7)
+ax4.set_xlabel('Time [s]')
+ax4.set_ylabel('Voltage [mV]')
+fig.show()
+
+plt.figure(dpi=200)
+plt.plot(SpikesN.t / second, SpikesN.i, '.k', ms=0.7)
+
+show()
+
+#%%
+
+# ------- SYNAPSES -------
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1,figsize=[10,12]) # Added figsize for better viewing
+
+
+# ax1.plot(SpikesN.t/second,spike_trains[0],'.g', ms=5,label='Spikes')
+ax1.plot(MonitorS.t / second, MonitorS[0].u_S, 'r', linewidth=0.7,label='Ready-to-relase resources')
+
+ax1.plot(MonitorS.t / second, MonitorS[0].x_S, 'c', linewidth=0.7,label='Available resources')
+
+
+ax1.legend()
+
+
+# ax2.plot(SpikesN.t/second,spike_trains[1],'.g', ms=5,label='Spikes')
+ax2.plot(MonitorS.t / second, MonitorS[1].u_S, 'r', linewidth=0.7)
+
+ax2.plot(MonitorS.t / second, MonitorS[1].x_S, 'c', linewidth=0.7)
+
+
+
+# ax3.plot(SpikesN.t/second,spike_trains[2],'.g', ms=5,label='Spikes')
+ax3.plot(MonitorS.t / second, MonitorS[2].u_S, 'r', linewidth=0.7)
+
+ax3.plot(MonitorS.t / second, MonitorS[2].x_S, 'c', linewidth=0.7)
+
+ax3.set_xlabel('Time [s]')
+# fig.show()
+
+# plt.figure(dpi=200)
+# plt.plot(SpikesN.t / second, SpikesN.i, '.k', ms=0.7)
+
+# show()
+
+#%%
+
+# Synaptically released glutamate 
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1,figsize=[10,12]) # Added figsize for better viewing
+
+
+
+
+ax1.plot(MonitorS.t / second, MonitorS[0].Y_S, 'k', linewidth=0.7,label='[Glu]')
+# ax1.plot(SpikesN.t/second,SpikesN[0].i+3,'.g', ms=5,label='Spikes')
+ax1.set_ylabel('Mol')
+ax1.legend()
+
+
+ax2.plot(MonitorS.t / second, MonitorS[1].Y_S, 'k', linewidth=0.7)
+# ax2.plot(SpikesN.t/second,SpikesN[1].i+3,'.g', ms=5)
+ax2.set_ylabel('Mol')
+
+
+ax3.plot(MonitorS.t / second, MonitorS[2].Y_S, 'k', linewidth=0.7)
+# ax3.plot(SpikesN.t/second,SpikesN[2].i+3,'.g', ms=5)
+# fig.show()
+ax3.set_ylabel('Mol')
+ax3.set_xlabel('Time [s]')
+# plt.figure(dpi=200)
+# plt.plot(SpikesN.t / second, SpikesN.i, '.k', ms=0.7)
+
+show()
+
+#%%
+
+# SYNAPTIC CURRENTS
+
+
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1,figsize=[10,12]) # Added figsize for better viewing
+
+
+
+ax1.plot(MonitorN.t/second, MonitorN[1].I_syn, 'k', label='I_syn')
+ax1.plot(MonitorN.t/second, MonitorN[1].I_ampa, 'r', label='I_ampa')
+ax1.plot(MonitorN.t/second, MonitorN[1].I_nmda, 'b', label='I_nmda')
+# ax1.plot(SpikesN.t/second, SpikesN[0].i+1e-11, '.g', ms=5, label='Spikes') # Or a more descriptive label like 'Neuron SpikesN'
+ax1.set_ylabel('A')
+ax1.legend()
+
+
+ax2.plot(MonitorN.t/second, MonitorN[2].I_syn, 'k')
+ax2.plot(MonitorN.t/second, MonitorN[2].I_ampa, 'r')
+ax2.plot(MonitorN.t/second, MonitorN[2].I_nmda, 'b')
+# ax2.plot(SpikesN.t/second,SpikesN[1].i+1e-11,'.g', ms=5)
+ax2.set_ylabel('A')
+
+
+ax3.plot(MonitorN.t/second, MonitorN[3].I_syn, 'k')
+ax3.plot(MonitorN.t/second, MonitorN[3].I_ampa, 'r')
+ax3.plot(MonitorN.t/second, MonitorN[3].I_nmda, 'b')
+# ax3.plot(SpikesN.t/second,SpikesN[2].i+1e-11,'.g', ms=5)
+# fig.show()
+ax3.set_ylabel('A')
+ax3.set_xlabel('Time [s]')
+# plt.figure(dpi=200)
+# plt.plot(SpikesN.t / second, SpikesN.i, '.k', ms=0.7)
+
+show()
+
 
